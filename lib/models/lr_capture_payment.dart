f14446031c4a58fb3183766906092e14c0e68938
Saksham Sharma <sakshamsharma630@gmail.com>
2025-03-20 10:43:58 +0530

    feat: add type and status in capture payment

--- lib/models/lr_capture_payment.dart
+++ lib/models/lr_capture_payment.dart
@@ -1,18 +1,42 @@
+enum PaymentType {
+  FIRST_PAYMENT,
+  WALLET_TOPUP,
+  FUNDS_WITHDRAWAL,
+  SUBSCRIPTION_CREATED,
+  SUBSCRIPTION_RENEWED,
+  DEFAULT_PAYMENT,
+  ONE_TIME,
+  RECURRING,
+}
+
+enum PaymentStatus {
+  PAYMENT_INITIATED,
+  PAYMENT_COMPLETED,
+  PAYMENT_FAILED,
+  PAYMENT_CANCELLED,
+}
+
 class LRCapturePayment {
   final String? paymentId;
   final String userId;
   final double amount;
+  final PaymentType? type;
+  final PaymentStatus? status;
 
   LRCapturePayment({
     this.paymentId,
     required this.userId,
     required this.amount,
+    this.type,
+    this.status,
   });
 
   Map<String, dynamic> toJSON() {
     Map<String, dynamic> json = {
       'user_id': userId,
       'amount': amount,
+      'type': type?.name,
+      'status': status?.name,
     };
 
     if (paymentId != null) {
